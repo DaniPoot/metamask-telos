de8633d01b927ba4f66d44dd3cb9cc29035e8fe0
DaniPoot <danielpoot.uc2@gmail.com>
2024-07-22 09:18:02 -0600

    🚀 feat(tokens.ts): refactor useTokensStore to use reactive Vue composition API for better reactivity and performance

--- store/tokens.ts
+++ store/tokens.ts
@@ -1,7 +1,35 @@
 import { defineStore } from 'pinia'
+import { ref, readonly } from 'vue'
 
-export const useTokensStore = defineStore('tokens', {
-  state: () => ({
-    tokens: []
-  }),
+type Token = {
+  contract: string
+  symbol: string
+  decimals: number
+  isFetching?: boolean
+}
+
+export const useTokensStore = defineStore('tokens', () => {
+  const tokens = ref<Token[]>([])
+
+  function addToken(accountAddress: string, { contract, symbol, decimals, isFetching }: Token) {
+    if (tokens.value.find((token) => token.contract === contract)) return
+    tokens.value.push({ contract, symbol, decimals, isFetching })
+    localStorage.setItem(`tokens-${accountAddress}`, JSON.stringify(tokens.value))
+  }
+
+  function getTokens(accountAddress: string) {
+    const storageTokens = localStorage.getItem(`tokens-${accountAddress}`) ? JSON.parse(localStorage.getItem(`tokens-${accountAddress}`)!) : []
+    tokens.value = storageTokens
+  }
+
+  function reset(accountAddress: string) {
+    tokens.value = []
+  }
+
+  return {
+    tokens: readonly(tokens),
+    addToken,
+    getTokens,
+    reset
+  }
 })
